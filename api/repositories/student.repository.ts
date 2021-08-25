efceae80835263a6afb4e25d27f44b841588fa9a
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-08-25 16:57:02 +0900

    update repository

--- api/repositories/student.repository.ts
+++ api/repositories/student.repository.ts
@@ -1,9 +1,8 @@
 import { NuxtAxiosInstance } from '@nuxtjs/axios';
 
-export const studentRepository = ($axios: NuxtAxiosInstance) => {
-  return {
-    findAll: () => $axios.$get('/api/students'),
-  };
-};
+export const studentRepository = ($axios: NuxtAxiosInstance) => ({
+  findAll: () => $axios.$get('/students'),
+  findById: (studentId: string) => $axios.$get(`/student/${studentId}`),
+});
 
 export type StudentRepository = ReturnType<typeof studentRepository>;
