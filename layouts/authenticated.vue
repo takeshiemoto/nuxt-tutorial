07d99f1bb7403f91cd792f8df5fd2640cbfeb91a
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-08-26 12:00:00 +0900

    dev

---
+++ layouts/authenticated.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Nav />
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+import Nav from '~/components/Nav.vue';
+
+export default defineComponent({
+  name: 'Authenticated',
+  components: { Nav },
+  setup() {
+    return {};
+  },
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,16 @@
 <template>
-  <div>
-    <Nav />
-    <StudentList :students="students" />
-  </div>
+  <StudentList :students="students" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api';
 
-import Nav from '~/components/Nav.vue';
 import StudentList from '~/components/StudentList.vue';
 import { useStudents } from '~/composables/useStudents';
 
 export default defineComponent({
-  components: { StudentList, Nav },
+  components: { StudentList },
+  layout: 'authenticated',
   setup() {
     const { students, fetchStudents } = useStudents();
 
